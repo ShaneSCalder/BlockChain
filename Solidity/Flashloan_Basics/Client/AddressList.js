4361aed238c5fb797244871b8ce391e48fe4f988
Shane Calder <shanescottcalder@gmail.com>
2022-06-12 13:14:28 -0700

    Create AddressList.js
    Router, factory and Token addresses.

---
+++ Solidity/Flashloan_Basics/Client/AddressList.js
@@ -0,0 +1,11 @@
+const addressFactory = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+const addressRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const addressFrom = ""; //Add token here 
+const addressTo = ""; // Add token here 
+
+module.exports = {
+    addressFactory,
+    addressRouter,
+    addressFrom,
+    addressTo,
+}
