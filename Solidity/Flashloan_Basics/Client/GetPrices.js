7d3121b9c2ccd8c6b1a7abe3d666f872f0977bf6
Shane Calder <shanescottcalder@gmail.com>
2022-06-12 14:48:11 -0700

    Update
    amountIn and amountOut tokens to provide price from blockchain.

--- Solidity/Flashloan_Basics/Client/GetPrices.js
+++ Solidity/Flashloan_Basics/Client/GetPrices.js
@@ -1,36 +1,56 @@
 const ethers = require("ethers");
 
 const {
-    addressFactory,
-    addressRouter,
-    addressFrom,
-    addressTo,
-} = require("./AddressList.js");
+  addressFactory,
+  addressRouter,
+  addressFrom,
+  addressTo,
+} = require("./AddressList");
 
-const {
-    erc20ABI,
-    factoryABI,
-    routerABI,
-    pairABI,
-} = require("./AbiList.js");
+const { erc20ABI, factoryABI, pairABI, routerABI } = require("./AbiList");
 
-// Standard Provider 
+// Standard Provider
 const provider = new ethers.providers.JsonRpcProvider(
-    "https://bsc-dataseed.binance.org/"
+  "https://bsc-dataseed.binance.org/"
 );
 
 // Connect to Factory
 const contractFactory = new ethers.Contract(
-    addressFactory,
-    factoryABI,
-    provider,
+  addressFactory,
+  factoryABI,
+  provider
 );
 
-// Connect to Router 
-const contractRouter = new ethers.Contract(
-    addressRouter,
-    factoryABI,
-    provider,
-);
+// Connect to Router
+const contractRouter = new ethers.Contract(addressRouter, routerABI, provider);
 
-console.log(contractRouter);
+// Call the Blockchain
+const getPrices = async (amountInHuman) => {
+  // Convert the amount in
+  const contractToken = new ethers.Contract(addressFrom, erc20ABI, provider);
+  const decimals = await contractToken.decimals();
+  const amountIn = ethers.utils.parseUnits(amountInHuman, decimals).toString();
+
+  // Get amounts out
+  const amountsOut = await contractRouter.getAmountsOut(amountIn, [
+    addressFrom,
+    addressTo,
+  ]);
+
+  // Convert amount out - decimals
+  const contractToken2 = new ethers.Contract(addressTo, erc20ABI, provider);
+  const decimals2 = await contractToken2.decimals();
+
+  // Convert amount out - human readable
+  const amountOutHuman = ethers.utils.formatUnits(
+    amountsOut[1].toString(),
+    decimals2
+  );
+
+  // Log output
+  console.log(amountOutHuman);
+};
+
+const amountInHuman = "500";
+getPrices(amountInHuman);
+
