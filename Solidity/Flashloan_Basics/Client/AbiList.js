fcd21bff8227c7c44673aedb2e1548c2cb014268
Shane Calder <shanescottcalder@gmail.com>
2022-06-12 14:45:47 -0700

    Update
    Updated getAmountsOut function.

--- Solidity/Flashloan_Basics/Client/AbiList.js
+++ Solidity/Flashloan_Basics/Client/AbiList.js
@@ -1,16 +1,19 @@
-const erc20ABI =["function decimals() external view returns (uint8)"];
+module.exports = { erc20ABI, factoryABI, pairABI, routerABI };
+
+const erc20ABI = ["function decimals() external view returns (uint8)"];
+
 const factoryABI = [
-    "function getPair(address tokenA, address tokenB) external view returns (address pair)", 
+  "function getPair(address tokenA, address tokenB) external view returns (address pair)",
 ];
 
 const pairABI = [
-    "function token0() external view returns (address)",
-    "function token1() external view returns (address)",
-    "function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)",
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  "function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)",
 ];
 
 const routerABI = [
-    "function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure virtual override returns (uint amountOut)",
+  "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
 ];
 
 module.exports = { erc20ABI, factoryABI, pairABI, routerABI };
