8587ec1f7c5213a5a8d36d300d844910801315e6
Shane Calder <shanescottcalder@gmail.com>
2022-06-13 21:27:13 -0700

    Update
    Updated to include correct code

--- Solidity/uniswapV2/Client/AbiList.js
+++ Solidity/uniswapV2/Client/AbiList.js
@@ -1,11 +1,19 @@
-const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH
-const addressTo = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // USDC
+const erc20ABI = ["function decimals() external view returns (uint8)"];
 
-module.exports = {
-  addressFactory,
-  addressRouter,
-  addressFrom,
-  addressTo,
-};
+const factoryABI = [
+  "function getPair(address tokenA, address tokenB) external view returns (address pair)",
+];
+
+const pairABI = [
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  "function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)",
+];
+
+const routerABI = [
+  "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+];
+
+module.exports = { erc20ABI, factoryABI, pairABI, routerABI };
+
+// Factory address > GetPair > 0x.... >
