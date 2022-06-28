f6fa26bf56bfb7ab7e4250edf24e640a8f904509
Shane Calder <shanescottcalder@gmail.com>
2022-06-28 15:37:31 -0600

    Create index.js
    Initial setup 
    
    Output will show:
    Balance 0
    Balance 2000000000

---
+++ Solana/Wallet/AirDrop/index.js
@@ -0,0 +1,47 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+//Send some Sol
+// to increase output change 2 to another number 
+const airDropSol = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        const latestBlockHash = await connection.getLatestBlockhash();
+        await connection.confirmTransaction({
+          blockhash: latestBlockHash.blockhash,
+          lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+          signature: fromAirDropSignature,
+        });
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+//Get balance 
+const main = async() => {
+    await getWalletBalance()
+    await airDropSol()
+    await getWalletBalance()
+}
+main()
