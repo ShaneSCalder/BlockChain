939cd99a94f8d2b996fd8d8dae09b0b7e01b4936
Shane Calder <shanescottcalder@gmail.com>
2022-07-08 15:25:48 -0400

    Create main.js
    Console log should create 0,1,2 hash and test if it is working.

---
+++ Bootcamp/WeekOne/main.js
@@ -0,0 +1,42 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data =data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    // Install npm install --save crypto-js
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "07/07/08", "Genesisblock", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let newCoinZee = new Blockchain();
+newCoinZee.addBlock(new Block(1, "07/07/2022", {amount: 8 }));
+newCoinZee.addBlock(new Block(2, "07/07/2022", {amount: 21 }));
+
+console.log(JSON.stringify(newCoinZee, null, 4));
